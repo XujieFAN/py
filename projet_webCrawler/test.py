2141f79f89e9124eeba13bb2a54a9f83bdfafb52
XujieFAN <fan.fr.1985@gmail.com>
2020-10-22 00:15:54 +0200

    chg to class

--- projet_webCrawler/test.py
+++ projet_webCrawler/test.py
@@ -1,43 +1,35 @@
-url = 'https://www.liaoxuefeng.com/wiki/896043488029600'
-link = '/wiki/896043488029600/900062620154944/111/222'
+from downloadHTML import DownloadHTML
+import requests
+from bs4 import BeautifulSoup
 
-urls=[]
-links=[]
+url = 'http://www.ityouknow.com/python/2019/07/31/python-plan-100.html'
+#url2 = 'https://www.liaoxuefeng.com/wiki/896043488029600/898732792973664'
 
-urls = str(url).split('/')
-links = str(link).split('/')
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36',
+            'Content-Type':'text/html',
+            'Accept-Language':'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5'}
+#path_selector = '#x-sidebar-left-content'
 
-print(urls)
-print(links)
+page1 = DownloadHTML(url, headers, 'test/','test1.html')
+#page2 = DownloadHTML(url2, headers, 'test/','test2.html')
 
-link = []
+#page1.downloadHtml()
+#page2.downloadHtml()
 
-_link = urls+links
-
-list(link.append(l) for l in _link if not l in link)
-
-a = '/'.join(link)
-
-print(link)
-print(a)
-
-
-
+res = requests.get(url, headers=headers)
+print(requests.Response)
 '''
-for i in str(url).split('/'):
-    urls.append(i)
+html = BeautifulSoup(res.text,'lxml')
 
-for i in str(link).split('/'):
-    links.append(i)
+#extract links, script
+[tag.extract() for tag in html.select('head link')]
+[tag.extract() for tag in html.select('head script')]
 
-print(urls)
-print(links)
+#for images
+SRCs_dict = page1.getImageSRCs_fromHTML(html)
+imgLocalNames = page1.getImages_fromSRCs(SRCs_dict.values())
+for imgSrc in html.find_all('img'):
+    imgSrc['src'] = imgLocalNames[SRCs_dict[imgSrc['src']]]
 
-lastIndex = 0
-
-for k in links:
-    if k in urls:
-        lastIndex = links.index(k)
-
-print(url+'/'+links[lastIndex+1])
+page1.html2file(str(html),page1.savedHtmlName)
 '''
